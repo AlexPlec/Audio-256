7aa6fcec4846c9ca0375f1f5d60bb05c20b3d376
AlexPlec <acchtc724@gmail.com>
2025-06-22 14:29:35 +0300

    Remove unused usings and add Mediator class
    Cleaned up multiple files by removing unnecessary using directives. Added a new internal Mediator class in the Core namespace.

--- Audio-256/UI/Artists/Controllers/ArtistListItemController.cs
+++ Audio-256/UI/Artists/Controllers/ArtistListItemController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Audio_256.UI.Artists.Controllers
+﻿namespace Audio_256.UI.Artists.Controllers
 {
     internal class ArtistListItemController
     {
--- Audio-256/UI/Artists/Models/ArtistsViewModel.cs
+++ Audio-256/UI/Artists/Models/ArtistsViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Audio_256.UI.Artists.Models
+﻿namespace Audio_256.UI.Artists.Models
 {
     internal class ArtistsViewModel
     {
+
     }
 }
